3cab7d52156ae263fcd3a3806fada49cfeafee4d
HÉCTOR OMAR QUINTERO GUTIÉRREZ <ho.quinterogutierrez@ugto.mx>
2021-09-30 20:56:21 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio/Laboratorio/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Laboratorio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+          
+        }
+
+        private void BotonConvertirFaC_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Resultado());
+
+
+        }
+    }
+}
---
+++ Laboratorio/Laboratorio/Resultado.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resultado : ContentPage
+    {
+        public Resultado()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnCalcularfac_Clicked(object sender, EventArgs e)
+        {
+            if(ValorFahren != null)
+            {
+                float fahrenheit = float.Parse(ValorFahren.Text);
+                float centigrados = (fahrenheit - 32f) * 5 / 9f;
+                centigrados = centigrados * 10f / 10f;
+
+                ResultadoFaC.Text = centigrados.ToString();
+            }
+
+           
+
+
+
+          
+        }
+
+        private void btnCalcularcaf_Clicked(object sender, EventArgs e)
+        {
+            if (ValorCentig != null)
+            {
+                float centigrados = float.Parse(ValorCentig.Text);
+                float fahrenheit = centigrados * 9f / 5 + 32f;
+                fahrenheit = fahrenheit * 10f / 10f;
+
+                ResultadoCaF.Text = fahrenheit.ToString();
+            }
+
+        }
+
+    }
+}
